715ebf4719abf108ed01d82a0771a9482362840b
Carlos Villa <carloslorenzovilla@gmail.com>
2019-11-25 00:57:08 -0800

    update

--- CH12.py
+++ CH12.py
@@ -1,10 +1,23 @@
 # -*- coding: utf-8 -*-
+def sumall(*args):
+    """ Returns the sum of all elements in a tuple of any length
 
+        args: tuple of numbers of any length
+
+        returns: int
+    """
+    return sum(*args)
 
 
 if __name__ == "__main__":
     
-    t2 = ()
-    t3 = tuple()
+    a = [1, 2, 3, 4]
+
+    for n in reversed(a):
+        print(n)
+
+    a.reverse()
+
+    print(a)
+
     
-    pass
