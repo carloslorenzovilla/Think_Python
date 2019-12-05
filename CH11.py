2d38a007100cc968bf8fc9af270f71cb121151fc
Carlos Villa <carloslorenzovilla@gmail.com>
2019-12-04 22:37:15 -0800

    Update

--- CH11.py
+++ CH11.py
@@ -32,7 +32,7 @@ def histogram(s):
     """
     d = {}
     for c in s:
-        d[c] = 1 + d.get(c, 0)
+        d[c] = d.get(c, 0) + 1
     return d
 
 #11-1
--- CH12.py
+++ CH12.py
@@ -8,16 +8,37 @@ def sumall(*args):
     """
     return sum(*args)
 
+#12-1
+
+def most_frequent(string_):
+    """ Returns letters in descending order of 
+        frequency.
+
+        string_: string of characters
+
+    """
+    count = {}
+    for char in string_:
+        count[char] = count.get(char, 0) + 1
+    
+    temp = []
+    for char, freq in count.items():
+        temp.append((freq, char))
+
+    temp.sort(reverse=True)
+
+    result = []
+    for freq, char in temp:
+        result.append(char)
+
+    print(*result)
+    
+
+
+#12-2
 
 if __name__ == "__main__":
     
-    a = [1, 2, 3, 4]
-
-    for n in reversed(a):
-        print(n)
-
-    a.reverse()
-
-    print(a)
+    most_frequent('zaanbnb asdkjfhaksdf aqwieur!')
 
     
